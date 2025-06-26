cd396add80bc4098d4a022617fc68861870948ad
Aryaa007 <aryaparihar0512@gmail.com>
2025-06-26 15:46:23 +0530

    update

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 
 model = joblib.load("delay_model.pkl")
-encoder = joblib.load("time_encoder.pkl")
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You can restrict this to ["http://127.0.0.1:5500"] for safety
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -22,13 +21,10 @@ app.add_middleware(
 #input structure
 class DeliveryInput(BaseModel):
     Distance_km: float
-    Delivery_Speed: float
     Agent_Rating: float
-    Agent_Efficiency: float
     Traffic_Level: int
     Weather_Impact: int
     Weekday: int
-    Time_of_Day: str
 
 
 @app.get("/")
@@ -40,19 +36,13 @@ def home():
 
 @app.post("/predict_delay")
 def predict_delay(data: DeliveryInput):
-    # Encode time_of_day
-    print(data)
-    time_encoded = encoder.transform([data.Time_of_Day])[0]
 
     features = np.array([
         data.Distance_km,
-        data.Delivery_Speed,
         data.Agent_Rating,
-        data.Agent_Efficiency,
         data.Traffic_Level,
         data.Weather_Impact,
         data.Weekday,
-        time_encoded
     ]).reshape(1, -1)
 
     prediction = model.predict(features)[0]
